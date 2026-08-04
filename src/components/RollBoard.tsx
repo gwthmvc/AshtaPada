1c203f5916a2b98b80c7ffa18c258bc1fa9cc9c5
gwthmvc <267390341+gwthmvc@users.noreply.github.com>
2026-08-04 10:58:23 -0400

    RollBoard updates

---
+++ src/components/RollBoard.tsx
@@ -0,0 +1,14 @@
+import "../styles/RollBoard.css"
+
+export default function RollBoard(){
+    return(
+        <>
+        <div className="RollBoard">
+            <div>
+                <h2>Roll Board</h2>
+                <div className="scoreBoard"></div>
+            </div>
+        </div>
+        </>
+    )
+}
