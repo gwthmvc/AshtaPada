8d6e2359476f9d667ce34e6ee5c908c996107344
gwthmvc <267390341+gwthmvc@users.noreply.github.com>
2026-08-04 10:39:59 -0400

    Added onClick function for button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,28 +1,21 @@
 import { useState } from "react";
-import "../styles/Button.css"
+import "../styles/Button.css";
 
 export default function Button() {
+    const [roll, setRoll] = useState(0);
 
-     function rollDice(){
-
+    function rollDice() {
         const diceRolls = [1, 2, 3, 4, 8];
-  
-        const [index, setIndex] = useState(0);
+        const randomIndex = Math.floor(Math.random() * diceRolls.length);
+        setRoll(diceRolls[randomIndex]);
+    }
 
-  };
-
-
-        return 
-
-        }
     return (
-
         <div className="button-container">
-            <button 
-            className='btn'
-            onClick={rollDice}
-            >Click to Roll</button>
+            <button className="btn" onClick={rollDice}>
+                Click to Roll
+            </button>
+            <span>Roll: {roll}</span>
         </div>
-    
-);
+    );
 }
