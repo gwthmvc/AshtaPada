3846a89bfdb455fafe19d485e85f9082174a60e1
gwthmvc <267390341+gwthmvc@users.noreply.github.com>
2026-08-04 11:01:43 -0400

    Added RollBoard to the GamePage

--- src/Pages/GamePage.tsx
+++ src/Pages/GamePage.tsx
@@ -2,15 +2,23 @@ import Header from "../components/Header";
 import Button from "../components/Button";
 import Board from "../components/Board";
 import "../styles/GamePage.css"
+import RollBoard from "../components/RollBoard";
 
 export default function GamePage(){
     return(
         <>
-        <div className="Gamepage">
-        <Header />
-        <Board />
-        <Button />
-        </div>
+        <div className="FullLayout">
+            <div className="PlayerBoard"></div>
+            <div className="Gamepage">
+            <Header />
+            <Board />
+            <Button />
+            </div>
+            <div className="ScoreBoard">
+                <RollBoard/>
+            </div>
+         </div>
         </>
+       
     )
 }
