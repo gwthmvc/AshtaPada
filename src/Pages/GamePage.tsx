86660f0d0794f99cbf7d66af48aa9a10b5ad140c
gwthmvc <267390341+gwthmvc@users.noreply.github.com>
2026-08-04 09:33:33 -0400

    Adjusted space

--- src/Pages/GamePage.tsx
+++ src/Pages/GamePage.tsx
@@ -1,13 +1,16 @@
 import Header from "../components/Header";
 import Button from "../components/Button";
 import Board from "../components/Board";
+import "../styles/GamePage.css"
 
 export default function GamePage(){
     return(
         <>
+        <div className="Gamepage">
         <Header />
         <Board />
         <Button />
+        </div>
         </>
     )
 }
