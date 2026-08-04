6d29852b1e2bf570667b5bbc2964505753a363f8
gwthmvc <267390341+gwthmvc@users.noreply.github.com>
2026-08-04 10:17:24 -0400

    Added Button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,10 +1,28 @@
+import { useState } from "react";
 import "../styles/Button.css"
 
 export default function Button() {
+
+     function rollDice(){
+
+        const diceRolls = [1, 2, 3, 4, 8];
+  
+        const [index, setIndex] = useState(0);
+
+  };
+
+
+        return 
+
+        }
     return (
+
         <div className="button-container">
-            <button className='btn'>Click to Roll</button>
+            <button 
+            className='btn'
+            onClick={rollDice}
+            >Click to Roll</button>
         </div>
     
-)
+);
 }
